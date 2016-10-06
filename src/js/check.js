50d4380df404c9e3e5b0e58f4dc14b75d7ed60e3
garold664 <garold664@gmail.com>
2016-10-06 20:46:51 +0300

    Переделал тело функции используя для проверки условия инструкцию if вместо инструкции switch

--- src/js/check.js
+++ src/js/check.js
@@ -1,39 +1,28 @@
 function getMessage(a, b) {
-  switch (typeof a) {
-    case "boolean":
-      if (a) {
-        return "Я попал в " + b;
-      }
-      return "Я никуда не попал";
 
-    case "number":
-      return "Я прыгнул на " + a * 100 + " сантиметров";
-
-    case "object":
-      if (Array.isArray(a) && Array.isArray(b)) {
-        var distancePath = 0;
-        for (var i = 0, maxA = a.length; i < maxA; i++) {
-          distancePath += a[i] * b[i];
-        }
-        return "Я прошёл " + distancePath + " метров";
-      } else if (!(Array.isArray(b))) {
-        var numberOfSteps = 0;
-        for (var i = 0, maxA = a.length; i < maxA; i++) {
-          numberOfSteps += a[i];
-        }
-        return "Я прошёл " + numberOfSteps + " шагов";
-      } else if (!(Array.isArray(a))) {
-        var numberOfSteps = 0;
-        for (var i = 0, maxB = b.length; i < maxB; i++) {
-          numberOfSteps += b[i];
-        }
-        return "Я прошёл " + numberOfSteps + " метров";
-      } else {
-        return "Переданы некорректные данные";
-      }
-    default:
-      return "Переданы некорректные данные";
+  if ( a === true) {
+    return "Я попал в " + b;
   }
+  if ( a === false) {
+    return "Я никуда не попал";
+  }
+  if (typeof a == "number") {
+    return "Я прыгнул на " + a * 100 + " сантиметров";
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var distancePath = 0;
+    for (var i = 0, l = a.length; i < l; i++) {
+      distancePath += a[i] * b[i];
+    }
+    return "Я прошёл " + distancePath + " метров";
+  }
+  if (Array.isArray(a) && !(Array.isArray(b))) {
+    var numberOfSteps = a.reduce(function(prev, curr) {
+      return prev + curr;
+    });
+    return "Я прошёл " + numberOfSteps + " шагов";
+  }
+  return "Переданы некорректные данные";
+
 }
-
-
