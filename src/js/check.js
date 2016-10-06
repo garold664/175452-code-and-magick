280caf5fd23251dd85948323558ab1da99d02195
garold664 <garold664@gmail.com>
2016-10-06 21:37:47 +0300

    Поправил оформление кода
    Заменил инструкцию for на метод reduce

--- src/js/check.js
+++ src/js/check.js
@@ -1,28 +1,31 @@
 function getMessage(a, b) {
+  if (typeof a === "boolean") {
+    if (a) {
+      return "Я попал в " + b;
+    }
 
-  if ( a === true) {
-    return "Я попал в " + b;
-  }
-  if ( a === false) {
     return "Я никуда не попал";
   }
-  if (typeof a == "number") {
+
+  if (typeof a === "number") {
     return "Я прыгнул на " + a * 100 + " сантиметров";
   }
 
   if (Array.isArray(a) && Array.isArray(b)) {
-    var distancePath = 0;
-    for (var i = 0, l = a.length; i < l; i++) {
-      distancePath += a[i] * b[i];
-    }
+    var distancePath = a.reduce(function(sum, item, i) {
+      return sum + (item * b[i]);
+    }, 0);
+
     return "Я прошёл " + distancePath + " метров";
   }
+
   if (Array.isArray(a) && !(Array.isArray(b))) {
-    var numberOfSteps = a.reduce(function(prev, curr) {
-      return prev + curr;
+    var numberOfSteps = a.reduce(function(sum, item) {
+      return sum + item;
     });
+
     return "Я прошёл " + numberOfSteps + " шагов";
   }
-  return "Переданы некорректные данные";
 
+  return "Переданы некорректные данные";
 }
