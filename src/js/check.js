dbc8d79624b26d1ca5181ab99296ba3fcb03f108
Garold664 <garold664@gmail.com>
2016-10-06 21:42:21 +0300

    Начинаем программировать (#2)
    * Выполнил 6-ое задание
    
    * Переделал тело функции используя для проверки условия инструкцию if вместо инструкции switch
    
    * Поправил оформление кода
    
    Заменил инструкцию for на метод reduce

---
+++ src/js/check.js
@@ -0,0 +1,31 @@
+function getMessage(a, b) {
+  if (typeof a === "boolean") {
+    if (a) {
+      return "Я попал в " + b;
+    }
+
+    return "Я никуда не попал";
+  }
+
+  if (typeof a === "number") {
+    return "Я прыгнул на " + a * 100 + " сантиметров";
+  }
+
+  if (Array.isArray(a) && Array.isArray(b)) {
+    var distancePath = a.reduce(function(sum, item, i) {
+      return sum + (item * b[i]);
+    }, 0);
+
+    return "Я прошёл " + distancePath + " метров";
+  }
+
+  if (Array.isArray(a) && !(Array.isArray(b))) {
+    var numberOfSteps = a.reduce(function(sum, item) {
+      return sum + item;
+    });
+
+    return "Я прошёл " + numberOfSteps + " шагов";
+  }
+
+  return "Переданы некорректные данные";
+}
