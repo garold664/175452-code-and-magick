3cc58c0c8a4e0ca9de15f23b277e1c4bc577686c
garold664 <garold664@gmail.com>
2016-10-12 20:48:31 +0300

    Сделал валидацию формы

--- src/js/form.js
+++ src/js/form.js
@@ -2,7 +2,18 @@
 
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
+  var reviewForm = document.querySelector('.review-form');
+  var reviewSubmit = document.querySelector('.review-submit');
   var formCloseButton = document.querySelector('.review-form-close');
+  var reviewGroupMark = document.querySelector('.review-form-group-mark');
+  var reviewFields = document.querySelector('.review-fields');
+  var reviewName = document.querySelector('#review-name');
+  var reviewLabelName = document.querySelector('.review-fields-name');
+  var reviewText = document.querySelector('#review-text');
+  var reviewLabelText = document.querySelector('.review-fields-text');
+  var reviewMark3 = document.querySelector('#review-mark-3');
+  var reviewMark4 = document.querySelector('#review-mark-4');
+  var reviewMark5 = document.querySelector('#review-mark-5');
 
   var form = {
     onClose: null,
@@ -24,6 +35,44 @@ window.form = (function() {
     }
   };
 
+
+  reviewSubmit.disabled = true;
+  reviewForm.onchange = function() {
+    if (this.checkValidity()) {
+      reviewFields.style.display = 'none';
+      reviewSubmit.disabled = false;
+    } else {
+      reviewFields.style.display = 'inline-block';
+      reviewSubmit.disabled = true;
+    }
+  };
+
+  reviewName.onchange = function() {
+    if (this.validity.valid) {
+      reviewLabelName.style.display = 'none';
+    } else {
+      reviewLabelName.style.display = 'inline-block';
+    }
+  };
+
+  reviewLabelText.style.display = 'none';
+  reviewText.onchange = function() {
+    if (this.validity.valid) {
+      reviewLabelText.style.display = 'none';
+    } else {
+      reviewLabelText.style.display = 'inline-block';
+    }
+  };
+
+  reviewGroupMark.onchange = function() {
+    if (!(reviewMark3.checked) && !(reviewMark4.checked) && !(reviewMark5.checked)) {
+      reviewText.required = true;
+      reviewFields.style.display = 'inline-block';
+      reviewLabelText.style.display = 'inline-block';
+    } else {
+      reviewText.required = false;
+    }
+  };
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
