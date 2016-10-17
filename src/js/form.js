2369890c8155b81141914bc585608fd05b191e2b
garold664 <garold664@gmail.com>
2016-10-17 15:05:11 +0300

    Сделал небольшой рефакторинг. Заменил анонимные функции на function declaration

--- src/js/form.js
+++ src/js/form.js
@@ -5,7 +5,6 @@ window.form = (function() {
   var reviewForm = document.querySelector('.review-form');
   var reviewSubmit = reviewForm.querySelector('.review-submit');
   var formCloseButton = reviewForm.querySelector('.review-form-close');
-  var reviewGroupMark = reviewForm.querySelector('.review-form-group-mark');
   var reviewFields = reviewForm.querySelector('.review-fields');
   var reviewName = reviewForm.querySelector('#review-name');
   var reviewLabelName = reviewForm.querySelector('.review-fields-name');
@@ -17,6 +16,10 @@ window.form = (function() {
 
   reviewSubmit.disabled = true;
   reviewLabelText.style.display = 'none';
+
+  formCloseButton.addEventListener('click', closeForm, false);
+  reviewForm.addEventListener('change', validateForm, false);
+
 
   var form = {
     onClose: null,
@@ -38,74 +41,38 @@ window.form = (function() {
     }
   };
 
-function validateForm(evt) {
+  function validateForm() {
+    if (!(reviewMark3.checked || reviewMark4.checked || reviewMark5.checked)) {
+      reviewText.required = true;
+    } else {
+      reviewText.required = false;
+    }
 
-  /*switch (evt.target.id) {
-    case 'review-name' :
+    if (reviewForm.checkValidity()) {
+      reviewFields.style.display = 'none';
+      reviewSubmit.disabled = false;
+    } else {
+      reviewFields.style.display = 'inline-block';
+      reviewSubmit.disabled = true;
+
       if (reviewName.validity.valid) {
         reviewLabelName.style.display = 'none';
       } else {
         reviewLabelName.style.display = 'inline-block';
       }
-      break;
 
-    case 'review-text' :
       if (reviewText.validity.valid) {
         reviewLabelText.style.display = 'none';
       } else {
         reviewLabelText.style.display = 'inline-block';
       }
-      break;
-
-    case 'review-mark-1' :
-    case 'review-mark-2' :
-    case 'review-mark-3' :
-    case 'review-mark-4' :
-    case 'review-mark-5' :
-      if (!(reviewMark3.checked || reviewMark4.checked || reviewMark5.checked)) {
-        reviewText.required = true;
-        reviewFields.style.display = 'inline-block';
-        reviewLabelText.style.display = 'inline-block';
-      } else {
-        reviewText.required = false;
-        reviewLabelText.style.display = 'none';
-      }
-      break;
-  }*/
-
-  if (!(reviewMark3.checked || reviewMark4.checked || reviewMark5.checked)) {
-    reviewText.required = true;
-  } else {
-    reviewText.required = false;
-  }
-
-  if (reviewForm.checkValidity()) {
-    reviewFields.style.display = 'none';
-    reviewSubmit.disabled = false;
-  } else {
-    reviewFields.style.display = 'inline-block';
-    reviewSubmit.disabled = true;
-
-    if (reviewName.validity.valid) {
-      reviewLabelName.style.display = 'none';
-    } else {
-      reviewLabelName.style.display = 'inline-block';
-    }
-
-    if (reviewText.validity.valid) {
-      reviewLabelText.style.display = 'none';
-    } else {
-      reviewLabelText.style.display = 'inline-block';
     }
   }
-}
 
-reviewForm.addEventListener('change', validateForm, false);
-
-  formCloseButton.onclick = function(evt) {
+  function closeForm(evt) {
     evt.preventDefault();
     form.close();
-  };
+  }
 
   return form;
 })();
