e935ace109d1ce7d2e7bc766efb6b85c5ba90133
Garold664 <garold664@gmail.com>
2016-10-17 22:09:11 +0300

    Валидация (#6)
    * Сделал валидацию формы
    
    * Оптимизировал поиск элементов через querySelector, Перекинул иницилиз-й код в начало файла
    
    * Сделал инверсию логического выражения
    
    * Добавил атрибут required к полю #review-name
    
    * Записал обработчик события change в виде отдельной ф-ии
    Удалил закомм-й код.
    
    * Переписал свою ф-ию избавившись от switch на чистых if-else
    
    * Сделал небольшой рефакторинг.
    Заменил анонимные функции на function declaration

--- src/js/form.js
+++ src/js/form.js
@@ -2,7 +2,24 @@
 
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
-  var formCloseButton = document.querySelector('.review-form-close');
+  var reviewForm = document.querySelector('.review-form');
+  var reviewSubmit = reviewForm.querySelector('.review-submit');
+  var formCloseButton = reviewForm.querySelector('.review-form-close');
+  var reviewFields = reviewForm.querySelector('.review-fields');
+  var reviewName = reviewForm.querySelector('#review-name');
+  var reviewLabelName = reviewForm.querySelector('.review-fields-name');
+  var reviewText = reviewForm.querySelector('#review-text');
+  var reviewLabelText = reviewForm.querySelector('.review-fields-text');
+  var reviewMark3 = reviewForm.querySelector('#review-mark-3');
+  var reviewMark4 = reviewForm.querySelector('#review-mark-4');
+  var reviewMark5 = reviewForm.querySelector('#review-mark-5');
+
+  reviewSubmit.disabled = true;
+  reviewLabelText.style.display = 'none';
+
+  formCloseButton.addEventListener('click', closeForm, false);
+  reviewForm.addEventListener('change', validateForm, false);
+
 
   var form = {
     onClose: null,
@@ -24,11 +41,38 @@ window.form = (function() {
     }
   };
 
+  function validateForm() {
+    if (!(reviewMark3.checked || reviewMark4.checked || reviewMark5.checked)) {
+      reviewText.required = true;
+    } else {
+      reviewText.required = false;
+    }
 
-  formCloseButton.onclick = function(evt) {
+    if (reviewForm.checkValidity()) {
+      reviewFields.style.display = 'none';
+      reviewSubmit.disabled = false;
+    } else {
+      reviewFields.style.display = 'inline-block';
+      reviewSubmit.disabled = true;
+
+      if (reviewName.validity.valid) {
+        reviewLabelName.style.display = 'none';
+      } else {
+        reviewLabelName.style.display = 'inline-block';
+      }
+
+      if (reviewText.validity.valid) {
+        reviewLabelText.style.display = 'none';
+      } else {
+        reviewLabelText.style.display = 'inline-block';
+      }
+    }
+  }
+
+  function closeForm(evt) {
     evt.preventDefault();
     form.close();
-  };
+  }
 
   return form;
 })();
