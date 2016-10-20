261cd7ffd3f3769155392c1aff0187cde1734dde
Garold664 <garold664@gmail.com>
2016-10-20 21:02:40 +0300

    Cookies (#7)
    * Подключил библиотеку cookie.js
    
    * Написал первоначальный вариант ф-ии для работы с куки
    
    * перекинул библиотеку по куки в папку lib
    
    * Закончил функцию для работы с Cookies
    
    * Исправил свои ошибки
    
    * исправил еще одну ошибку
    
    * убрал console.log() из кода

--- src/js/form.js
+++ src/js/form.js
@@ -19,7 +19,9 @@ window.form = (function() {
 
   formCloseButton.addEventListener('click', closeForm, false);
   reviewForm.addEventListener('change', validateForm, false);
-
+  reviewForm.addEventListener('input', validateForm, false);
+  reviewForm.addEventListener('submit', setCookiesFromForm, false);
+  window.addEventListener('load', fillFormFromCookies, false);
 
   var form = {
     onClose: null,
@@ -40,6 +42,20 @@ window.form = (function() {
       }
     }
   };
+
+
+  function fillFormFromCookies() {
+    var reviewMarkFromCookies = window.Cookies.get('review-mark');
+    var reviewNameFromCookies = window.Cookies.get('review-name');
+    var reviewMarkToCheck;
+
+    reviewName.value = reviewNameFromCookies || '';
+
+    if (reviewMarkFromCookies) {
+      reviewMarkToCheck = document.getElementById(reviewMarkFromCookies);
+      reviewMarkToCheck.checked = true;
+    }
+  }
 
   function validateForm() {
     if (!(reviewMark3.checked || reviewMark4.checked || reviewMark5.checked)) {
@@ -74,5 +90,27 @@ window.form = (function() {
     form.close();
   }
 
+  function setCookiesFromForm() {
+    var today = new Date();
+    var birthdayOfGraceHopper = new Date();
+    var diff;
+    var name = reviewName.value;
+    var reviewMarkChecked = reviewForm.elements['review-mark'].value;
+    var millisecondsInDay = (1000 * 60 * 60 * 24);
+
+    birthdayOfGraceHopper.setMonth(11);
+    birthdayOfGraceHopper.setDate(9);
+
+    if (today.getTime() < birthdayOfGraceHopper.getTime()) {
+      birthdayOfGraceHopper.setFullYear(today.getFullYear() - 1);
+    }
+    diff = today.getTime() - birthdayOfGraceHopper.getTime();
+    diff = Math.floor(diff / millisecondsInDay);
+
+    window.Cookies.set('review-name', name, { expires: diff });
+    window.Cookies.set('review-mark', reviewMarkChecked, { expires: diff });
+  }
+
   return form;
 })();
+
