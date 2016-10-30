9c93ef35e522a6ecc0e572769ba260a930f9d08e
Garold664 <garold664@gmail.com>
2016-10-30 17:24:57 +0300

    Больше модулей (#10)
    * Сделал подключение по AMD модулей form.js, game.js, reviews.js к модулю main.js
    
    * разделил модуль reviews на 3 модуля по AMD

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,6 +1,6 @@
 'use strict';
 
-(function() {
+define(['./review', './load'], function(compileReview, load) {
   var REVIEWS_LOAD_URL = 'http://localhost:1507/api/reviews';
   var template = document.querySelector('#review-template');
   var templateContainer = 'content' in template ? template.content : template;
@@ -9,22 +9,7 @@
   var reviewsFilter = document.querySelector('.reviews-filter');
 
   hideElement(reviewsFilter);
-  handleJSONPResponse(REVIEWS_LOAD_URL, showReviews);
-
-
-  function handleJSONPResponse(url, callback, callbackName) {
-    if (!callbackName) {
-      callbackName = 'cb' + Date.now();
-    }
-
-    window[callbackName] = function(data) {
-      callback(data);
-    };
-
-    var script = document.createElement('script');
-    script.src = url + '?callback=' + callbackName;
-    document.body.appendChild(script);
-  }
+  load(REVIEWS_LOAD_URL, showReviews);
 
   function showReviews(data) {
     renderReviews(data);
@@ -33,35 +18,9 @@
 
   function renderReviews(reviews) {
     reviews.forEach(function(review) {
-      var reviewElement = compileReview(review);
+      var reviewElement = compileReview(review, templateReviewElement);
       reviewsList.appendChild(reviewElement);
     });
-  }
-
-  function compileReview(review) {
-    var IMAGE_SIZE = 124;
-    var reviewElement = templateReviewElement.cloneNode(true);
-    var reviewAuthor = reviewElement.querySelector('.review-author');
-    var reviewRating = reviewElement.querySelector('.review-rating');
-    var reviewText = reviewElement.querySelector('.review-text');
-    var image = new Image();
-
-    image.onload = function() {
-      reviewAuthor.width = IMAGE_SIZE;
-      reviewAuthor.height = IMAGE_SIZE;
-      reviewAuthor.src = image.src;
-    };
-
-    image.onerror = function() {
-      reviewElement.classList.add('review-load-failure');
-    };
-
-    image.src = review.author.picture;
-    reviewAuthor.title = review.author.name;
-    reviewRating.textContent = review.rating;
-    reviewText.textContent = review.description;
-
-    return reviewElement;
   }
 
   function showElement(element) {
@@ -71,5 +30,5 @@
   function hideElement(element) {
     element.classList.add('invisible');
   }
-})();
+});
 
