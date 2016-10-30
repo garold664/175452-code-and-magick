911ab669dccfc83bfd5e62f5a090f5ea740b6fd8
garold664 <garold664@gmail.com>
2016-10-30 23:04:04 +0300

    изменил названия свойств конструктора Gallery

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -3,34 +3,34 @@
 define(function() {
   function Gallery(pictures) {
     this.pictures = pictures;
-    this.overlayGallery = document.querySelector('.overlay-gallery');
-    this.overlayGalleryPreview = this.overlayGallery.querySelector('.overlay-gallery-preview');
-    this.overlayGalleryControlLeft = this.overlayGallery.querySelector('.overlay-gallery-control-left');
-    this.overlayGalleryControlRight = this.overlayGallery.querySelector('.overlay-gallery-control-right');
-    this.previewNumberCurrent = this.overlayGallery.querySelector('.preview-number-current');
-    this.previewNumberTotal = this.overlayGallery.querySelector('.preview-number-total');
-    this.overlayGalleryClose = this.overlayGallery.querySelector('.overlay-gallery-close');
+    this.overlay = document.querySelector('.overlay-gallery');
+    this.preview = this.overlay.querySelector('.overlay-gallery-preview');
+    this.buttonPrevious = this.overlay.querySelector('.overlay-gallery-control-left');
+    this.buttonNext = this.overlay.querySelector('.overlay-gallery-control-right');
+    this.currentPictureNumber = this.overlay.querySelector('.preview-number-current');
+    this.picturesQuantity = this.overlay.querySelector('.preview-number-total');
+    this.buttonClose = this.overlay.querySelector('.overlay-gallery-close');
   }
 
   Gallery.prototype.setActivePicture = function(currentNumber) {
     this.activePicture = currentNumber;
     var img = new Image();
     img.src = this.pictures[currentNumber - 1];
-    this.overlayGalleryPreview.removeChild(this.overlayGalleryPreview.lastChild);
-    this.overlayGalleryPreview.appendChild(img);
-    this.previewNumberCurrent.innerText = currentNumber;
+    this.preview.removeChild(this.preview.lastChild);
+    this.preview.appendChild(img);
+    this.currentPictureNumber.innerText = currentNumber;
 
   };
   Gallery.prototype.show = function(currentNumber) {
     var self = this;
 
-    this.overlayGallery.classList.remove('invisible');
-    this.previewNumberTotal.innerText = this.pictures.length;
-    this.overlayGalleryClose.onclick = function() {
+    this.overlay.classList.remove('invisible');
+    this.picturesQuantity.innerText = this.pictures.length;
+    this.buttonClose.onclick = function() {
       self.hide();
     };
 
-    this.overlayGalleryControlLeft.onclick = function() {
+    this.buttonPrevious.onclick = function() {
       if (self.activePicture > 1) {
         self.setActivePicture(self.activePicture - 1);
       } else {
@@ -38,7 +38,7 @@ define(function() {
       }
     };
 
-    this.overlayGalleryControlRight.onclick = function() {
+    this.buttonNext.onclick = function() {
       if (self.activePicture < self.pictures.length) {
         self.setActivePicture(self.activePicture + 1);
       } else {
@@ -50,11 +50,11 @@ define(function() {
   };
 
   Gallery.prototype.hide = function() {
-    this.overlayGallery.classList.add('invisible');
+    this.overlay.classList.add('invisible');
 
-    this.overlayGalleryClose.onclick = null;
-    this.overlayGalleryControlLeft.onclick = null;
-    this.overlayGalleryControlRight.onclick = null;
+    this.buttonClose.onclick = null;
+    this.buttonPrevious.onclick = null;
+    this.buttonNext.onclick = null;
   };
 
   return Gallery;
