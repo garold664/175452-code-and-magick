2233e184993f306a44727fa755777c397b769fff
Garold664 <garold664@gmail.com>
2016-11-08 19:58:39 +0300

    Постраничный вывод и делегирование событий (#13)
    * Сделал постраничный вывод отзывов с фильтрацией
    
    * небольшие поправки
    
    * добавил в функцию фильтрации отзывов фильтрацию по дате добавления
    
    * немного подправил оформление кода
    
    * оптимизировал названия методов showElement, hideElement до show, hide
    
    * убрал аргумент filterID из ф-ии loadPage
    
    * убрал аргумент currentPageNumber из ф-ии loadPage()
    
    * добавил удаление обработчиков событий при применении фильтра. Еще несколько улучшений
    
    * оптимизировал switch

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -1,5 +1,63 @@
 'use strict';
 
 module.exports = function(list, filterID) {
-  return list;
+  switch (filterID) {
+    case 'reviews-all':
+      return fetchAll(list);
+
+    case 'reviews-recent':
+      return fetchRecent(list);
+
+    case 'reviews-good':
+      return fetchGood(list);
+
+    case 'reviews-bad':
+      return fetchBad(list);
+
+    case 'reviews-popular':
+      return fetchPopular(list);
+  }
+
+function fetchAll(array) {
+  return array;
+}
+
+function fetchRecent(array) {
+  return array.filter(function(item) {
+    var createdTime = item.created;
+    var now = Date.now();
+    var millisecondsInDay = (1000 * 60 * 60 * 24);
+    var elapsedDays = (now - createdTime)/millisecondsInDay;
+
+    return elapsedDays <= 3;
+  }).sort(function(a, b) {
+    return b.created - a.created;
+  });
+}
+
+function fetchGood(array) {
+  return array.filter(function(item) {
+    return item.rating >= 3;
+  }).sort(function(a, b) {
+    return b.rating - a.rating;
+  });
+}
+
+function fetchBad(array) {
+  return array.filter(function(item) {
+    return item.rating < 3;
+  }).sort(function(a, b) {
+    return a.rating - b.rating;
+  });
+}
+
+function fetchPopular(array) {
+  return array.sort(function(a, b) {
+    return b.review_usefulness - a.review_usefulness;
+  });
+}
 };
+
+
+
+
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,36 +1,82 @@
 'use strict';
 
 define(['./review', './load'], function(Review, load) {
-  var REVIEWS_LOAD_URL = 'http://localhost:1507/api/reviews';
   var template = document.querySelector('#review-template');
   var templateContainer = 'content' in template ? template.content : template;
   var templateReviewElement = templateContainer.firstElementChild;
   var reviewsList = document.querySelector('.reviews-list');
   var reviewsFilter = document.querySelector('.reviews-filter');
+  var moreReviewsBtn = document.querySelector('.reviews-controls-more');
 
+  var filterID = 'reviews-all';
+  var pageNumber = -1;
+  var pageSize = 3;
+  var instancesOfReview = [];
 
-  hideElement(reviewsFilter);
-  load(REVIEWS_LOAD_URL, showReviews);
+  hide(reviewsFilter);
+  loadPage();
+
+  show(moreReviewsBtn);
+  moreReviewsBtn.addEventListener('click', showMoreReviews);
+  reviewsFilter.addEventListener('change', applyFilter, true);
+
+  function applyFilter(evt) {
+    var target = evt.target;
+    if (target.type !== 'radio') {
+      return;
+    }
+    reviewsList.innerHTML = '';
+    instancesOfReview.forEach(function(review) {
+      review.remove();
+    });
+    instancesOfReview = [];
+
+    filterID = target.id;
+    pageNumber = -1;
+    loadPage();
+    show(moreReviewsBtn);
+  }
+
+  function showMoreReviews() {
+    loadPage();
+  }
+
+  function loadPage() {
+
+    var REVIEWS_LOAD_URL = '/api/reviews';
+    var fromItem = ++pageNumber * pageSize;
+    var toItem = fromItem + pageSize;
+
+    load(REVIEWS_LOAD_URL, {
+      from: fromItem,
+      to: toItem,
+      filter: filterID
+    }, showReviews);
+  }
 
   function showReviews(data) {
     renderReviews(data);
-    showElement(reviewsFilter);
+    show(reviewsFilter);
   }
 
   function renderReviews(reviews) {
+    if (reviews.length < pageSize) {
+      hide(moreReviewsBtn);
+    }
+
     reviews.forEach(function(item) {
       var review = new Review(item, templateReviewElement.cloneNode(true));
+      instancesOfReview.push(review);
       reviewsList.appendChild(review.element);
     });
   }
 
-  function showElement(element) {
+  function show(element) {
     element.classList.remove('invisible');
   }
 
-  function hideElement(element) {
+  function hide(element) {
     element.classList.add('invisible');
   }
-
 });
 
