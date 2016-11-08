b07aa8409936d5704d1994804776de9cb2e26b3d
garold664 <garold664@gmail.com>
2016-11-08 16:01:05 +0300

    оптимизировал switch

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -3,41 +3,59 @@
 module.exports = function(list, filterID) {
   switch (filterID) {
     case 'reviews-all':
-      return list;
+      return fetchAll(list);
 
     case 'reviews-recent':
-      return list.filter(function(item) {
-        var createdTime = item.created;
-        var now = Date.now();
-        var millisecondsInDay = (1000 * 60 * 60 * 24);
-        var elapsedDays = (now - createdTime)/millisecondsInDay;
-
-        return elapsedDays <= 3;
-      }).sort(function(a, b) {
-        return b.created - a.created;
-      });
+      return fetchRecent(list);
 
     case 'reviews-good':
-      return list.filter(function(item) {
-        return item.rating >= 3;
-      }).sort(function(a, b) {
-        return b.rating - a.rating;
-      });
+      return fetchGood(list);
 
     case 'reviews-bad':
-      return list.filter(function(item) {
-        return item.rating < 3;
-      }).sort(function(a, b) {
-        return a.rating - b.rating;
-      });
+      return fetchBad(list);
 
     case 'reviews-popular':
-      return list.sort(function(a, b) {
-        return b.review_usefulness - a.review_usefulness;
-      });
+      return fetchPopular(list);
   }
 
-  return list;
+function fetchAll(array) {
+  return array;
+}
+
+function fetchRecent(array) {
+  return array.filter(function(item) {
+    var createdTime = item.created;
+    var now = Date.now();
+    var millisecondsInDay = (1000 * 60 * 60 * 24);
+    var elapsedDays = (now - createdTime)/millisecondsInDay;
+
+    return elapsedDays <= 3;
+  }).sort(function(a, b) {
+    return b.created - a.created;
+  });
+}
+
+function fetchGood(array) {
+  return array.filter(function(item) {
+    return item.rating >= 3;
+  }).sort(function(a, b) {
+    return b.rating - a.rating;
+  });
+}
+
+function fetchBad(array) {
+  return array.filter(function(item) {
+    return item.rating < 3;
+  }).sort(function(a, b) {
+    return a.rating - b.rating;
+  });
+}
+
+function fetchPopular(array) {
+  return array.sort(function(a, b) {
+    return b.review_usefulness - a.review_usefulness;
+  });
+}
 };
 
 
