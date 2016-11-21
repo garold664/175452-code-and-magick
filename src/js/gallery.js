c5c67a73add066279ce1d88c677c9cc5bbc6dd2d
garold664 <garold664@gmail.com>
2016-11-21 22:06:11 +0300

    поправил ошибки и убрал отладочный код

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -8,12 +8,18 @@ define(['./inherit', './base-component'], function(inherit, BaseComponent) {
     this.pictures = pictures;
     this.overlay = document.querySelector('.overlay-gallery');
     this.preview = this.overlay.querySelector('.overlay-gallery-preview');
+
     this.buttonPrevious = this.overlay.querySelector('.overlay-gallery-control-left');
     this.buttonNext = this.overlay.querySelector('.overlay-gallery-control-right');
     this.currentPictureNumber = this.overlay.querySelector('.preview-number-current');
     this.picturesQuantity = this.overlay.querySelector('.preview-number-total');
     this.buttonClose = this.overlay.querySelector('.overlay-gallery-close');
 
+    this.photoContainer = document.querySelector('.photogallery');
+    this.lastParent = this.photoContainer.querySelector('a:last-of-type');
+    this.firstParent = this.photoContainer.querySelector('a:first-of-type');
+
+    this.render = this.render.bind(this);
     this.onHashChange = this.onHashChange.bind(this);
 
     window.addEventListener('hashchange', this.onHashChange);
@@ -23,8 +29,6 @@ define(['./inherit', './base-component'], function(inherit, BaseComponent) {
   Gallery.prototype.onHashChange = function() {
     var hash = String.prototype.match.call(location.hash, /#photo\/(\S+)/);
     if (hash !== null) {
-      console.log(hash[1]);
-      console.log(location.origin.length);
       this.render(hash[1]);
     } else {
       this.remove();
@@ -61,14 +65,12 @@ define(['./inherit', './base-component'], function(inherit, BaseComponent) {
   Gallery.prototype.showPrevious = function() {
     if (typeof this.activePicture !== 'number') {
       var currentPic = document.querySelector('img[src="' + this.activePicture + '"]');
-      console.log(currentPic);
       var parent = currentPic.parentNode;
-      console.log(parent);
       var prevParent = parent.previousElementSibling;
+
       prevParent = ((prevParent) && (prevParent.tagName === 'A')) ? prevParent : this.lastParent;
       var prevPic = prevParent.firstElementChild;
-      console.log(prevParent);
-      console.log(prevPic);
+
       location.hash = 'photo/' + prevPic.src.substr(location.origin.length + 1);
     } else {
       var index = (this.activePicture > 1) ? this.activePicture - 1 : this.pictures.length;
@@ -79,14 +81,12 @@ define(['./inherit', './base-component'], function(inherit, BaseComponent) {
   Gallery.prototype.showNext = function() {
     if (typeof this.activePicture !== 'number') {
       var currentPic = document.querySelector('img[src="' + this.activePicture + '"]');
-      console.log(currentPic);
       var parent = currentPic.parentNode;
-      console.log(parent);
-
       var nextParent = parent.nextElementSibling;
+
       nextParent = ((nextParent) && (nextParent.tagName === 'A')) ? nextParent : this.firstParent;
       var nextPic = nextParent.firstElementChild;
-      console.log(nextPic);
+
       location.hash = 'photo/' + nextPic.src.substr(location.origin.length + 1);
     } else {
       var index = (this.activePicture < this.pictures.length) ? this.activePicture + 1 : 1;
@@ -114,6 +114,8 @@ define(['./inherit', './base-component'], function(inherit, BaseComponent) {
     this.buttonClose.removeEventListener('click', this.remove);
     this.buttonPrevious.removeEventListener('click', this.showPrevious);
     this.buttonNext.removeEventListener('click', this.showNext);
+
+    location.hash = '';
   };
 
   return Gallery;
